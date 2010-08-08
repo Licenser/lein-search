1a7879225f4b7fc79d8f6b1b766c4b273e470bdf
Steve Purcell <steve@sanityinc.com>
2010-08-08 21:19:07 +0800

    Distribute my fork with a different group ID on clojars

--- project.clj
+++ project.clj
@@ -1,1 +1,7 @@
-(defproject lein-search "0.3.0-SNAPSHOT" :description "Leinigen plugin to search and add dependencies from clojars" :dependencies ([org.clojure/clojure "[1.1,1.2]"] [org.clojure/clojure-contrib "[1.1,1.2]"]) :dev-dependencies ([lein-clojars "0.5.0"] [swank-clojure/swank-clojure "1.1.0"]))
+(defproject org.clojars.sanityinc/lein-search
+  "0.3.0-SNAPSHOT"
+  :description "Leinigen plugin to search and add dependencies from clojars"
+  :dependencies [[org.clojure/clojure "[1.1,1.2.0-master-SNAPSHOT]"]
+                 [org.clojure/clojure-contrib "[1.1,1.2.0-SNAPSHOT]"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]
+                     [swank-clojure/swank-clojure "1.1.0"]])
