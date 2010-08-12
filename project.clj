b5f3c0c2aa2b57976f9b7ab4fd3bedd609977ba0
Heinz N. Gies <heinz@licenser.net>
2010-08-12 19:44:32 +0200

    Redone commit b5ee4c07a1678c31f50b from purcell, fixing typo in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-search "0.3.1" 
-	:description "Leinigen plugin to search and add dependencies from clojars" 
+(defproject lein-search "0.3.2" 
+	:description "Leiningen plugin to search and add dependencies from clojars"
 	:dependencies [[org.clojure/clojure "1.1.0"] [org.clojure/clojure-contrib "1.1.0"]]
 	:dev-dependencies [[lein-clojars "0.5.0"] [swank-clojure/swank-clojure "1.1.0"]])
