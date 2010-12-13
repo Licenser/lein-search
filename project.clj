864b971cca0af110019d47dbf4e620d59c32eef0
Heinz N. Gies <heinz@licenser.net>
2010-12-13 19:44:46 +0100

    Updated to newer swank and lein-clojars deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-search "0.3.3" 
+(defproject lein-search "0.3.4" 
 	:description "Leiningen plugin to search and add dependencies from clojars"
-	:dependencies [[org.clojure/clojure "1.1.0"] [org.clojure/clojure-contrib "1.1.0"]]
-	:dev-dependencies [[lein-clojars "0.5.0"] [swank-clojure/swank-clojure "1.1.0"]])
+	:dependencies [[org.clojure/clojure "1.2.0"] [org.clojure/clojure-contrib "1.2.0"]]
+	:dev-dependencies [[lein-clojars "0.6.0"] [swank-clojure "1.2.1"]])
